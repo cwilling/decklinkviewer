7be1b31c39738b4d4ec29c7f5786e5d37eb4d9c8
Christoph Willing <c.willing@uq.edu.au>
2012-12-17 10:27:02 +1000

    add copyright statements

--- displaywindow.h
+++ displaywindow.h
@@ -1,5 +1,24 @@
+/* ex:set ai shiftwidth=4 inputtab=spaces smarttab noautotab: */
+
 #ifndef DISPLAY_WINDOW_H
 #define DISPLAY_WINDOW_H
+
+/*
+This file is part of decklinkviewer.
+
+decklinkviewer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+decklinkviewer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with decklinkviewer.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <QtGui>
 
